7f535cc1bb476c8031c0d97739cac6cd61276a6a
Felipe López Valdez <0204021@up.edu.mx>
2021-02-09 20:02:34 -0700

    game of life rules implemented

--- GoL/conway.py
+++ GoL/conway.py
@@ -3,68 +3,67 @@ conway.py
 A simple Python/matplotlib implementation of Conway's Game of Life.
 """
 
-import sys, argparse
+import sys
+import argparse
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-ON = 255
-OFF = 0
-vals = [ON, OFF]
 
-def randomGrid(N):
-    """returns a grid of NxN random values"""
-    return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+def addBlinker(grid: np.ndarray, i: int, j: int) -> None:
+    blinker = np.array([[255], [255], [255]])
+    n, m = blinker.shape
+    grid[i:i+n, j:j+m] = blinker
 
-def addGlider(i, j, grid):
-    """adds a glider with top left cell at (i, j)"""
-    glider = np.array([[0,    0, 255], 
-                       [255,  0, 255], 
-                       [0,  255, 255]])
-    grid[i:i+3, j:j+3] = glider
 
-def update(frameNum, img, grid, N):
-    # copy grid since we require 8 neighbors for calculation
-    # and we go line by line 
-    newGrid = grid.copy()
-    # TODO: Implement the rules of Conway's Game of Life
+def checkRulesOfLife(grid: np.ndarray, i: int, j: int) -> int:
+    live_neighbors = 0
+    for y in range(-1, 2):
+        for x in range(-1, 2):
+            if y == 0 and x == 0:
+                continue
+            if grid[i+y, j+x] == 255:
+                live_neighbors += 1
+    if grid[i, j] == 255 and (live_neighbors == 2 or live_neighbors == 3):
+        return 255
+    elif grid[i, j] == 0 and live_neighbors == 3:
+        return 255
+    return 0
 
-    # update data
-    img.set_data(newGrid)
-    grid[:] = newGrid[:]
-    return img,
 
-# main() function
-def main():
-    # Command line args are in sys.argv[1], sys.argv[2] ..
-    # sys.argv[0] is the script name itself and can be ignored
-    # parse arguments
-    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life system.py.")
-    # TODO: add arguments
-    
-    # set grid size
-    N = 100
-        
-    # set animation update interval
-    updateInterval = 50
+def iterateGrid(grid: np.ndarray) -> np.ndarray:
+    newGrid = np.copy(grid)
+    n, m = grid.shape
+    for y in range(1, n-1):
+        for x in range(1, m-1):
+            newGrid[y, x] = checkRulesOfLife(grid, y, x)
+    return newGrid
 
-    # declare grid
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Runs Conway's Game of Life system.py.")
+    N = 10
     grid = np.array([])
-    # populate grid with random on/off - more off than on
-    grid = randomGrid(N)
-    # Uncomment lines to see the "glider" demo
-    #grid = np.zeros(N*N).reshape(N, N)
-    #addGlider(1, 1, grid)
+    grid = np.zeros(N*N).reshape(N, N)
+    addBlinker(grid, 2, 2)
 
-    # set up animation
     fig, ax = plt.subplots()
-    img = ax.imshow(grid, interpolation='nearest')
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
-                                  frames = 10,
-                                  interval=updateInterval,
-                                  save_count=50)
-
+    ax.imshow(grid, interpolation='nearest')
     plt.show()
+
+    grid = np.copy(iterateGrid(grid))
+
+    fig, ax = plt.subplots()
+    ax.imshow(grid, interpolation='nearest')
+    plt.show()
+
+    grid = np.copy(iterateGrid(grid))
+
+    fig, ax = plt.subplots()
+    ax.imshow(grid, interpolation='nearest')
+    plt.show()
+
 
 # call main
 if __name__ == '__main__':

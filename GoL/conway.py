25363cdc3e85d20fbdcf8535bd5405f2b324ff49
Felipe López Valdez <0204021@up.edu.mx>
2021-02-11 19:22:41 -0700

    input reader added

--- GoL/conway.py
+++ GoL/conway.py
@@ -3,17 +3,28 @@ conway.py
 A simple Python/matplotlib implementation of Conway's Game of Life.
 """
 
+from io import FileIO
 import sys
 import argparse
+from typing import IO
 import numpy as np
+import matplotlib.image as mltimg
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from numpy.core.fromnumeric import repeat
+from configurations import *
 
 
-def addBlinker(grid: np.ndarray, i: int, j: int) -> None:
-    blinker = np.array([[255], [255], [255]])
-    n, m = blinker.shape
-    grid[i:i+n, j:j+m] = blinker
+def update(frameNum: int, img: mltimg.AxesImage, grid: np.ndarray, N: int, M: int):
+    # copy grid since we require 8 neighbors for calculation
+    # and we go line by line
+    newGrid = iterateGrid(grid)
+    # TODO: Implement the rules of Conway's Game of Life
+
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
 
 
 def checkRulesOfLife(grid: np.ndarray, i: int, j: int) -> int:
@@ -35,6 +46,7 @@ def checkRulesOfLife(grid: np.ndarray, i: int, j: int) -> int:
 
 
 def iterateGrid(grid: np.ndarray) -> np.ndarray:
+    configurationsPF = {'blinker': 0}
     newGrid = np.copy(grid)
     n, m = grid.shape
     for y in range(0, n):
@@ -43,31 +55,40 @@ def iterateGrid(grid: np.ndarray) -> np.ndarray:
     return newGrid
 
 
+def readInput(f: IO):
+    N, M = [int(x) for x in f.readline().split(' ')]
+    FRAMES = int(f.readline())
+    grid = np.zeros(N*M).reshape(N, M)
+
+    while True:
+        l = f.readline()
+        if not l:
+            break
+        x, y = [int(x) for x in l.split(' ')]
+        n, m = grid.shape
+        if x >= 0 and x < n and y >= 0 and y < m:
+            grid[x, y] = 255
+
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, M,),
+                                  frames=FRAMES, repeat=True)
+    plt.show()
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(
         description="Runs Conway's Game of Life system.py.")
-    N = 10
-    grid = np.array([])
-    grid = np.zeros(N*N).reshape(N, N)
-    addBlinker(grid, 2, 2)
 
-    fig, ax = plt.subplots()
-    ax.imshow(grid, interpolation='nearest')
-    plt.show()
+    file = None
+    try:
+        file = open(sys.argv[1])
+    except:
+        print('An error has occurred')
 
-    grid = np.copy(iterateGrid(grid))
-
-    fig, ax = plt.subplots()
-    ax.imshow(grid, interpolation='nearest')
-    plt.show()
-
-    grid = np.copy(iterateGrid(grid))
-
-    fig, ax = plt.subplots()
-    ax.imshow(grid, interpolation='nearest')
-    plt.show()
+    if file:
+        readInput(file)
 
 
-# call main
 if __name__ == '__main__':
     main()

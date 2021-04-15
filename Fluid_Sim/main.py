bf5887b6578b5b974d93cbb07523567793ab9c59
Felipe López Valdez <0204021@up.edu.mx>
2021-04-14 21:19:26 -0600

    Add media examples

--- Fluid_Sim/main.py
+++ Fluid_Sim/main.py
@@ -1,7 +1,10 @@
 from typing import List
 from fluid import Fluid
+from matplotlib import animation
+import matplotlib.pyplot as plt
 import math
 import sys
+import time
 
 velocities_positions = []
 velocities_properties = []
@@ -21,23 +24,31 @@ def input_file_reader(path: str, f: Fluid) -> None:
     file_lines = file.readlines().copy()
 
     # Read simulation color
-    s = file_lines.pop(0).split(" ")
-    for c in s:
-        color.append(int(c))
-    n = int(file_lines.pop(0))
+    try:
+        s = file_lines.pop(0).split(" ")
+        for c in s:
+            color.append(int(c))
+        n = int(file_lines.pop(0))
+    except:
+        print("ERROR: No color provided")
 
     # Read sources of density
     for _ in range(n):
-        s = file_lines.pop(0).split(" ")
-        x, y = [int(x) for x in s]
-        if densities.__contains__((x, y)):
-            print(
-                "ERROR: There is already a source of density in that position ->", (x, y))
-        elif x > f.size or x < 0 or y > f.size or y < 0:
-            print("ERROR: Position out of bounds ->", (x, y))
-        else:
-            densities.append((x, y))
-    print("Current sources of density ->", densities)
+        try:
+            s = file_lines.pop(0).split(" ")
+            x, y = [int(x) for x in s]
+            if densities.__contains__((x, y)):
+                print(
+                    "ERROR: There is already a source of density in that position ->", (x, y))
+            elif x > f.size or x < 0 or y > f.size or y < 0:
+                print("ERROR: Position out of bounds ->", (x, y))
+            else:
+                densities.append((x, y))
+        except:
+            print("ERROR: The number of densities not matches with the ones provided")
+
+    if len(file_lines) == 0:
+        return
 
     # Read sources of velocity
     n = int(file_lines.pop(0))
@@ -59,8 +70,9 @@ def input_file_reader(path: str, f: Fluid) -> None:
                 velocities_positions.append((x, y))
                 velocities_properties.append(
                     (name, int(s[3]), int(s[4]), int(s[5]), int(s[6])))
-    print("Current sources of velocity ->", velocities_positions)
 
+    if len(file_lines) == 0:
+        return
     # Read objects
     n = int(file_lines.pop(0))
     for _ in range(n):
@@ -83,6 +95,9 @@ def input_file_reader(path: str, f: Fluid) -> None:
             elif name == 'circle':
                 objects_positions.append((x, y))
                 objects_properties.append((name, int(s[3])))
+    print("Current sources of density positions ->", densities)
+    print("Current sources of velocity positions ->", velocities_positions)
+    print("Current objects positions ->", objects_positions)
 
 
 def get_color_image(c: List[int], intensity: float, m: int, n: int, f: Fluid) -> List[List[int]]:
@@ -93,9 +108,20 @@ def get_color_image(c: List[int], intensity: float, m: int, n: int, f: Fluid) ->
             r = (c[0] / 255.0) * (f.density[i, j] / intensity)
             g = (c[1] / 255.0) * (f.density[i, j] / intensity)
             b = (c[2] / 255.0) * (f.density[i, j] / intensity)
+            r = clamp(float(r), 1, 0)
+            g = clamp(float(g), 1, 0)
+            b = clamp(float(b), 1, 0)
             row.append([r, g, b])
         pixels.append(row)
     return pixels
+
+
+def clamp(value: float, maximum: int, minimun: int):
+    if value > maximum:
+        return maximum
+    elif value < minimun:
+        return minimun
+    return value
 
 
 def spinner_animation(period: int, direction: int, f: int) -> List[int]:
@@ -144,8 +170,6 @@ def add_circle(r: int, x0: int, y0: int, fluid: Fluid):
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    from matplotlib import animation
 
     inst = Fluid()
 
@@ -200,10 +224,12 @@ if __name__ == "__main__":
 
         # Calculate and assign the image color
         m, n = inst.density.shape
-        maxs = []
+        density_values = []
         for i in range(m):
-            maxs.append(max(inst.density[i]))
-        image = get_color_image(color, max(maxs), m, n, inst)
+            for j in range(n):
+                if inst.density[i, j] != 0:
+                    density_values.append(inst.density[i, j])
+        image = get_color_image(color, max(density_values), m, n, inst)
 
         # Update the outputs
         im.set_array(image)
@@ -224,14 +250,15 @@ if __name__ == "__main__":
 
     # Write and creates video file
 
-    """ name, extension = file_name.split(".")
-        writervideo = animation.FFMpegWriter(fps=30)
+    """writervideo = animation.FFMpegWriter(fps=30)
         anim.save("movie.mp4", writer=writervideo)
-        pillow = animation.PillowWriter(fps=30)
-        print("Start Date Time: ", time.ctime())
-        currTime = time.time()
-        anim.save(name+".gif", pillow)
-        print("End Date Time: ", time.ctime())
         print("Duration: ", time.time()-currTime, "s") """
+
+    name, extension = file_name.split(".")
+    pillow = animation.PillowWriter(fps=30)
+    print("Start Date Time: ", time.ctime())
+    currTime = time.time()
+    anim.save(name+".gif", pillow)
+    print("End Date Time: ", time.ctime())
 
     plt.show()
